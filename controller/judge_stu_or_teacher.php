bb0587757e88ae16db01f30f26a28829ec64be20
huangwenqing <huangwenqing@baidu.com>
2016-06-15 10:53:06 +0800

    reconstruct

---
+++ controller/judge_stu_or_teacher.php
@@ -0,0 +1,24 @@
+<?php
+    require_once("../models/dal_api.php");
+	session_start();
+    if(!isset($_SESSION['user_id'])){
+	    echo '<html>'; 
+	    echo '<head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /></head>'; 
+	    echo "<script type='text/javascript'>alert('请先登录!');</script>";
+	    echo "<script type='text/javascript'>window.location.href='index.php'</script>";
+	    echo '</html>'; 
+    }else{
+    	if($_SESSION['user_type'] == "s"){	
+    		$_SESSION["stu_id"] = $_SESSION["user_id"];
+    		#设置开始时间
+    		$dal_api  = new nc_dal_adapter();
+    		$dal_api->set_chess_start_time($_SESSION["user_id"]);
+    		header("Location:classroom.html");
+    	}else if($_SESSION['user_type'] == "t"){
+			header("Location:/view/tea_info.php");
+		}else{
+            header("Location:/view/admin_info.php");
+        }
+    	
+    }
+?>
---
+++ view/js/index.js
@@ -0,0 +1,120 @@
+﻿
+jQuery(function(){
+    $ = jQuery ;
+    //main menu
+    banner_height = $(window).height() - $('#templatemo_banner_menu').outerHeight();
+    $('#templatemo_about').height(banner_height);
+    $('#templatemo_events').height(banner_height);
+    $('#templatemo_timeline').height(banner_height);
+    $('#templatemo_contact').height(banner_height);
+    $('#templatemo_class').height(banner_height);
+    $("#templatemo_banner_menu ul").singlePageNav({offset:$('#templatemo_banner_menu').outerHeight()});
+    //banner slide
+    $('.banner').unslider({fluid: true});
+    $(window).on("load scroll resize", function(){
+        banner_height = $(window).height() - $('#templatemo_banner_menu').outerHeight();
+        $('.banner').height(banner_height);
+        $('.banner ul li').height(banner_height);
+        if(banner_height > 250){
+            caption_margin_top = (banner_height-100)/2;
+            $('.banner .slide_caption:hidden').show();
+            $('.banner .slide_caption').css({"margin-top":caption_margin_top});
+        }else{
+            $('.banner .slide_caption').hide();
+        }
+        $("#templatemo_banner_slide > ul > li").css({"background-size":"100% 100%"});
+    });
+   //about icon
+    $(window).on("load scroll resize", function(){
+        about_wap_width = $(".about_icon").width();
+        about_icon_padding_left = (about_wap_width/100)*30;
+        about_icon_width = (about_wap_width/100)*40;
+        about_icon_size = (about_icon_width/100)*50;
+        about_icon_padding_top = (about_icon_width/100)*25;
+        $(".about_icon .imgwap").css({
+                                                    'margin-left': about_icon_padding_left,
+                                                    'width': about_icon_width,
+                                                    'height': about_icon_width
+                                                    });
+    });
+    $(window).on("load resize", function(){
+        $.timeline_right_position_top = 0 ;
+        $.timeline_old_right_position_top = 0 ;
+        $.timeline_left_position_top = 0 ;
+        $.timeline_old_left_position_top = 0 ;
+        w_width = ($(window).width()>1600) ? 1600 : $(window).width() ;
+        $.timeline_item_width = ( w_width - 50) / 2;
+        $(".time_line_wap").each(function(){
+            //if class name already exit remove
+            $(this).children("a.left_timer").remove();
+            $(this).children("a.right_timer").remove();
+            $(this).removeClass("left_timeline");
+            $(this).removeClass("right_timeline");
+            if($(window).width()<970){
+                $("#templatemo_timeline .container-fluid").css({"position":"absolute"});
+                positon_left = $("#templatemo_timeline .container-fluid").position().left +100;
+                //put on right
+                $(this).css({   
+                                    'left': 70,
+                                    'top':$.timeline_right_position_top,
+                                    'width': $(window).width() - positon_left
+                                 });
+                $(this).addClass("right_timeline");
+                $.timeline_old_right_position_top = $.timeline_right_position_top;
+                $.timeline_right_position_top = $.timeline_right_position_top + $(this).outerHeight() + 40 ;
+                $(this).prepend("<a href=\"#\" class=\"right_timer\"><span class=\"glyphicon glyphicon-time\"></span></a>");
+                $(this).children("a.right_timer").css({left:-86, width: 60});
+            }else if($.timeline_left_position_top == 0){
+                $("#templatemo_timeline .container-fluid").css({"position":"relative"});
+                //put on left
+                $(this).css({   
+                                    'left':0,
+                                    'top':0,
+                                    'width': $.timeline_item_width - 50
+                                 });
+                $(this).addClass("left_timeline");
+                $.timeline_old_left_position_top = $.timeline_left_position_top;
+                $.timeline_left_position_top = $.timeline_left_position_top + $(this).outerHeight() + 40 ;
+                $(this).prepend("<a href=\"#\" class=\"left_timer\"><span class=\"glyphicon glyphicon-time\"></span></a>");
+                $(this).children("a.left_timer").css({left:$.timeline_item_width-50});
+            }else if( $.timeline_right_position_top < $.timeline_left_position_top ){
+                $("#templatemo_timeline .container-fluid").css({"position":"relative"});
+                $.timeline_right_position_top = ($.timeline_old_left_position_top + 40) < $.timeline_right_position_top  ? $.timeline_right_position_top : $.timeline_right_position_top + 40;
+                //put on right
+                $(this).css({   
+                                    'left': $.timeline_item_width + 79,
+                                    'top':$.timeline_right_position_top,
+                                    'width': $.timeline_item_width - 50
+                                 });
+                $(this).addClass("right_timeline");
+                $.timeline_old_right_position_top = $.timeline_right_position_top;
+                $.timeline_right_position_top = $.timeline_right_position_top + $(this).outerHeight() + 40 ;
+                $(this).prepend("<a href=\"#\" class=\"right_timer\"><span class=\"glyphicon glyphicon-time\"></span></a>");
+                $(this).children("a.right_timer").css({left:-99});
+            }else{
+                $("#templatemo_timeline .container-fluid").css({"position":"relative"});
+                $.timeline_left_position_top = ($.timeline_old_right_position_top + 40) < $.timeline_left_position_top ? $.timeline_left_position_top : $.timeline_left_position_top + 40;
+                //put on left
+                $(this).css({
+                                    'left':0,
+                                    'top':$.timeline_left_position_top,
+                                    'width': $.timeline_item_width - 50
+                                 });
+                $(this).addClass("left_timeline");
+                $.timeline_old_left_position_top = $.timeline_left_position_top;
+                $.timeline_left_position_top = $.timeline_left_position_top + $(this).outerHeight() + 40 ;
+                $(this).prepend("<a href=\"#\" class=\"left_timer\"><span class=\"glyphicon glyphicon-time\"></span></a>");
+                $(this).children("a.left_timer").css({left:$.timeline_item_width-50});
+            }
+            //calculate and define container height
+            if($.timeline_left_position_top > $.timeline_right_position_top ){
+                $("#templatemo_timeline .container-fluid").height($.timeline_left_position_top-40);
+                $("#templatemo_timeline").height($.timeline_left_position_top+200);
+            }else{
+                $("#templatemo_timeline .container-fluid").height($.timeline_right_position_top-40);
+                $("#templatemo_timeline").height($.timeline_right_position_top+200);
+            }
+            $(this).fadeIn();
+        });
+    });
+});
